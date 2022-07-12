891646067497f29e9b2679d043ddb1195ea68ff5
curation <adam@curations-Air.lan>
2022-07-12 13:04:39 +0100

    feat: add graph utils

--- graph_paths/dodecahedron.py
+++ graph_paths/dodecahedron.py
@@ -1,95 +1,73 @@
-import pprint
-from collections import defaultdict
+import graphs.utils as gr
+import numpy as np
+from pprint import PrettyPrinter
+import matplotlib.plt as plt
+printer = PrettyPrinter()
 
 
-class Graph(object):
-    """ Graph data structure, undirected by default. """
+connections = [
+    (0, 1), (0, 2), (0, 3),
+    # (1, 0), only edges out of home node, not back (don't want paths that go 'through' the home node)
+    (1, 4), (1, 5), 
+    # (2, 0), 
+    (2, 6), (2, 7),
+    # (3, 0),
+    (3, 8), (3, 9),
+    (4, 1), (4, 9), (4, 10),
+    (5, 1), (5, 6), (5, 11),
+    (6, 5), (6, 2), (6, 12),
+    (7, 2), (7, 8), (7, 13),
+    (8, 3), (8, 7), (8, 14),
+    (9, 3), (9, 4), (9, 15),
+    (10, 4), (10, 11), (10, 16),
+    (11, 5), (11, 10), (11, 17),
+    (12, 6), (12, 17), (12, 13),
+    (13, 7), (13, 18), (13, 12),
+    (14, 8), (14, 15), (14, 18),
+    (15, 9), (15, 14), (15, 16),
+    (16, 10), (16, 15), (16, 19),
+    (17, 11), (17, 12), (17, 19),
+    (18, 13), (18, 19), (18, 14),
+    (19, 16), (19, 18), (19, 17),
+]
+g = gr.graph(connections)
 
-    def __init__(self, connections, directed=False):
-        self._graph = defaultdict(set)
-        self._directed = directed
-        self.add_connections(connections)
+adjacency = gr.adjacency(g)
+for i in range(len(adjacency)):
+    """
+    special adjacency matrix - we don't want paths that go through node 0
+    so we make A[i][0] for all i - ie: pretend there is no edge between i and 0
+    we only want paths that begin with 0 and end at 1, 2 or 3
+    """
+    adjacency[i][0] = 0
 
-    def add_connections(self, connections):
-        """ Add connections (list of tuple pairs) to graph """
+res = np.identity(len(g))
+exp = 0
+prob = 1 / 3
+total_prob = 0
+print(adjacency)
 
-        for node1, node2 in connections:
-            self.add(node1, node2)
+def get_num_paths(adjacency):
+    return int(sum([res[0][1], res[0][2], res[0][3]]))
+    
 
-    def add(self, node1, node2):
-        """ Add connection between node1 and node2 """
+for n in range(1, 700):
+    N = n + 1
+    np.matmul(adjacency, res, out=res)
+    num_distinct_paths = get_num_paths(res)
+    print('num paths', num_distinct_paths, N)
+    prob = prob/3
 
-        self._graph[node1].add(node2)
-        if not self._directed:
-            self._graph[node2].add(node1)
+    probability = num_distinct_paths * prob
+    total_prob += probability
 
-    def remove(self, node):
-        """ Remove all references to node """
-
-        for n, cxns in self._graph.items():  # python3: items(); python2: iteritems()
-            try:
-                cxns.remove(node)
-            except KeyError:
-                pass
-        try:
-            del self._graph[node]
-        except KeyError:
-            pass
-
-    def is_connected(self, node1, node2):
-        """ Is node1 directly connected to node2 """
-
-        return node1 in self._graph and node2 in self._graph[node1]
-
-    def find_path(self, node1, node2, path=[]):
-        """ Find any path between node1 and node2 (may not be shortest) """
-
-        path = path + [node1]
-        if node1 == node2:
-            return path
-        if node1 not in self._graph:
-            return None
-        for node in self._graph[node1]:
-            if node not in path:
-                new_path = self.find_path(node, node2, path)
-                if new_path:
-                    return new_path
-        return None
-
-    def __str__(self):
-        return '{}({})'.format(self.__class__.__name__, dict(self._graph))
-
-def graph(connections):
-    g = defaultdict(set)
-    for node1, node2 in connections:
-        g[node1].add(node2)
-        g[node2].add(node1)
-    return g
+    exp += N * probability
 
 
-g = graph(
-    [
-        (0, 1), (0, 2), (0, 3),
-        (1, 0), (1, 4), (1, 5),
-        (2, 0), (2, 6), (2, 7),
-        (3, 0), (3, 8), (3, 9),
-        (4, 1), (4, 9), (4, 10),
-        (5, 1), (5, 6), (5, 11),
-        (6, 5), (6, 2), (6, 12),
-        (7, 2), (7, 8), (7, 13),
-        (8, 3), (8, 7), (8, 14),
-        (9, 3), (9, 4), (9, 15),
-        (10, 4), (10, 11), (10, 16),
-        (11, 5), (11, 10), (11, 17),
-        (12, 6), (12, 17), (12, 13),
-        (13, 7), (13, 18), (13, 12),
-        (14, 8), (14, 15), (14, 18),
-        (15, 9), (15, 14), (15, 16),
-        (16, 10), (16, 15), (16, 19),
-        (17, 11), (17, 12), (17, 19),
-        (18, 13), (18, 19), (18, 14),
-        (19, 16), (19, 18), (19, 17),
-    ]
-)
+print('expected value', exp, total_prob)
+print('--------')
+# expected value is ~19.999999
+"""
+can probably concentrate on B >= 20
 
-print(g[9])
+"""
